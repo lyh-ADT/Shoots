d8ecf38306ce0db7c7f0faf18050ee3db7fcb929
lyh-ADT <1043970238@qq.com>
2020-05-11 23:30:26 +0800

    added shooter.py, Shooter class

---
+++ Shoots/bin/shooter.py
@@ -0,0 +1,42 @@
+from Shoots.bin.map import Info,Map
+
+class Shooter:
+    def __init__(self, map:Map):
+        self.face = Info.FACE_UP # the direction of shooter's face
+        self.vision = 5 # vision radius
+        self.listen = 10 # listen radius
+        self.cd = 3 # shot cool down tick
+        self.cd_count = 0
+        self.info = None # infomation of environment(sound, vision)
+        self.position = (0, 0)
+        self.map = map
+    
+    def update_info(self, info:Info):
+        self.info = info
+    
+    def get_info(self):
+        return self.info
+
+    def shoot(self):
+        if self.cd_count > 0:
+            return
+    
+    def move_up(self):
+        np = (self.position[0]-1, self.position[1])
+        if self.map.is_road(np):
+            self.position = np
+
+    def move_down(self):
+        np = (self.position[0]+1, self.position[1])
+        if self.map.is_road(np):
+            self.position = np
+
+    def move_left(self):
+        np = (self.position[0], self.position[1]-1)
+        if self.map.is_road(np):
+            self.position = np
+
+    def move_right(self):
+        np = (self.position[0], self.position[1]+1)
+        if self.map.is_road(np):
+            self.position = np
